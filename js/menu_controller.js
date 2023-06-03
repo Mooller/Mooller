a8557bb93446452ce38a083da054988f2c48cf21
PauCuminal <pauueet@gmail.com>
2023-06-03 16:39:18 +0200

    Afegir menu amb vue i css
    Menu principal fet, falta fer la animacio del svg i del menu ja estar tot

--- js/menu_controller.js
+++ js/menu_controller.js
@@ -1,12 +1,21 @@
-var menu = new Vue({
-    el: '#menu',
-    data: {
+var menu = function(){
+	var vue_instance = new Vue({
+		el: "#menuP",
+		
+		methods: { 
+			start_game: function(){
+				 window.location.href = "html/game.html";
+			},
+			exit: function(){
+				if (confirm("Sortir?")) {
+					window.location.href = "https://moodle2.udg.edu/course/view.php?id=33309";
+				  }
+			},
+			options: function(){
+				window.location.href ="./html/options.html";
+			},
 
-    },
-    methods: {
-        play: function () {
-            console.log("funciona");
-            window.location.href = "html/game.html";
-        }
-    }
-})
+		}
+	});
+
+}();
