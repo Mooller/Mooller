b855a5653f21a32a84b57c4daf3422ec9c59a1a4
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2023-06-04 10:52:32 +0200

    Crosshair added
    :angel:

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -1,18 +1,24 @@
 class GameScene extends Phaser.Scene {
     constructor() {
         super(GameScene);
-
+        this.ch = null;
     }
 
     preload() {
-
+        this.load.image('ch', '../resources/crosshair.png');
     }
 
     create() {
+        let canvas = this.sys.canvas;
+        canvas.style.cursor = 'none';
+
+        this.ch = this.physics.add.sprite(300, 300, 'ch');
+        this.ch.setScale(0.25);
+
 
     }
 
     update() {
-
+        this.ch.setPosition(this.input.x, this.input.y);
     }
 }
