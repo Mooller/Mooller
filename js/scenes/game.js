55c37b1b5953bf29fce5fe6ef36492bb60013e8b
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2023-06-04 14:55:02 +0200

    Canvis arreglats

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -12,6 +12,7 @@ class GameScene extends Phaser.Scene {
         this.textPoints = null;
         this.points = 0;
         this.move = 1;
+        this.vides = 5;
     }
 
     preload() {
@@ -20,9 +21,10 @@ class GameScene extends Phaser.Scene {
         this.load.image('fonsFront', '../recources/Fons_joc_Frotn.png');
         this.load.image('baca', '../recources/vaka.gif');
         this.load.image('badbaca', '../recources/gifgit.gif');
+        this.load.image('marc', '../recources/Marcs.png');
         this.load.audio('deadcow1', '../recources/audio/dead_cow_1.mp3');
         this.load.audio('deadcow2', '../recources/audio/dead_cow_2.mp3');
-        this.load.audio('paw', '../recources/audio/paw3.mp3');
+        this.load.audio('paw', '../recources/audio/paw.mp3');
     }
 
     create() {
@@ -30,7 +32,11 @@ class GameScene extends Phaser.Scene {
         canvas.style.cursor = 'none';
 
         this.add.image(500, 400, 'fonsBack').setDepth(2);
-        this.add.image(500, 400, 'fonsFront').setDepth(3)
+        this.add.image(500, 400, 'fonsFront').setDepth(3);
+
+        this.add.image(160, 70, 'marc').setDepth(2).setScale(0.4);
+        this.add.image(500, 70, 'marc').setDepth(2).setScale(0.4);
+        this.add.image(840, 70, 'marc').setDepth(2).setScale(0.4);
 
         this.baques = this.physics.add.staticGroup();
         this.baques.create(500, 550, 'baca').setScale(0.2);
@@ -55,7 +61,9 @@ class GameScene extends Phaser.Scene {
         this.deadCow2 = this.sound.add('deadcow2');
         this.paw = this.sound.add('paw');
 
-        this.textPoints = this.add.text(0, 0, 'Punts: 0', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setDepth(30);
+        this.textPoints = this.add.text(100, 50, 'Punts: 0', { fontFamily: 'Lilita One, Arial, Helvetica, sans-serif', fontSize: 30 }).setDepth(30);
+        this.textTime = this.add.text(460, 50, '00:00', { fontFamily: 'Lilita One, Arial, Helvetica, sans-serif', fontSize: 30 }).setDepth(30);
+        this.textLives = this.add.text(780, 50, 'Vides 5/5', { fontFamily: 'Lilita One, Arial, Helvetica, sans-serif', fontSize: 30 }).setDepth(30);
 
         this.input.on('pointerup', () => { this.shoot() });
     }
@@ -96,13 +104,15 @@ class GameScene extends Phaser.Scene {
 
             if (tipus == "baca") {
                 this.points--;
+                this.vides--;
                 this.textPoints.setText("Punts: " + this.points);
+                this.textLives.setText("Vides " + this.vides + "/5");
             }
             else if (tipus == "badbaca") {
                 this.points++;
                 this.textPoints.setText("Punts: " + this.points);
             }
             obj.destroy();
-        }, 700);
+        }, 100);
     }
 }
