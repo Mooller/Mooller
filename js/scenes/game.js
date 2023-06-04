19fbf3f3234aef978ac7cf9f368740877750e1b6
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2023-06-04 11:27:13 +0200

    Sound effect afegits

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -4,6 +4,8 @@ class GameScene extends Phaser.Scene {
         this.ch = null;
         this.baques = null;
         this.badbaques = null;
+
+        this.deadCow1 = null;
     }
 
     preload() {
@@ -11,6 +13,7 @@ class GameScene extends Phaser.Scene {
         this.load.image('fons', '../recources/Fons_joc.png');
         this.load.image('baca', '../recources/vaka.gif');
         this.load.image('badbaca', '../recources/gifgit.gif');
+        this.load.audio('deadcow1', '../recources/audio/dead_cow_1.mp3');
     }
 
     create() {
@@ -24,12 +27,22 @@ class GameScene extends Phaser.Scene {
 
         this.badbaques = this.physics.add.staticGroup();
         this.badbaques.create(400, 600, 'badbaca').setScale(0.2);
+        this.badbaques.children.iterate((badbaca) => {
+            badbaca.setInteractive();
+            badbaca.on('pointerup', () => this.matarBaca())
+        })
 
         this.ch = this.physics.add.sprite(300, 300, 'ch');
         this.ch.setScale(0.25);
+
+        this.deadCow1 = this.sound.add('deadcow1');
     }
 
     update() {
         this.ch.setPosition(this.input.x, this.input.y);
+    }
+
+    matarBaca() {
+        this.deadCow1.play();
     }
 }
