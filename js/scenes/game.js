1113e742d9e2f0cd71f30865ac224ed60a1ae7f2
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2023-06-04 11:05:49 +0200

    Fons ficat

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -6,11 +6,14 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image('ch', '../recources/crosshair.png');
+        this.load.image('fons', '../recources/Fons_joc.png');
     }
 
     create() {
         let canvas = this.sys.canvas;
         canvas.style.cursor = 'none';
+
+        this.add.image(500, 400, 'fons');
 
         this.ch = this.physics.add.sprite(300, 300, 'ch');
         this.ch.setScale(0.25);
