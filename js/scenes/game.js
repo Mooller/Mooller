1ddf7859615b0072d12995ec16a00586c9eb268b
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2023-06-04 15:46:58 +0200

    Generacio de baques

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -11,7 +11,6 @@ class GameScene extends Phaser.Scene {
 
         this.textPoints = null;
         this.points = 0;
-        this.move = 1;
         this.vides = 5;
     }
 
@@ -39,20 +38,10 @@ class GameScene extends Phaser.Scene {
         this.add.image(840, 70, 'marc').setDepth(2).setScale(0.4);
 
         this.baques = this.physics.add.staticGroup();
-        this.baques.create(500, 550, 'baca').setScale(0.2);
-        this.baques.children.iterate((baca) => {
-            baca.setInteractive();
-            baca.on('pointerup', () => this.matarBaca('baca', baca))
-            baca.setDepth(2);
-        });
-
         this.badbaques = this.physics.add.staticGroup();
-        this.badbaques.create(1100, 700, 'badbaca').setScale(0.2);
-        this.badbaques.children.iterate((badbaca) => {
-            badbaca.setInteractive();
-            badbaca.on('pointerup', () => this.matarBaca('badbaca', badbaca))
-            badbaca.setDepth(5);
-        });
+        this.craerBadBaca();
+        this.crearBaca();
+
 
         this.ch = this.physics.add.sprite(300, 300, 'ch').setDepth(10);
         this.ch.setScale(0.25);
@@ -72,19 +61,64 @@ class GameScene extends Phaser.Scene {
         this.ch.setPosition(this.input.x, this.input.y);
 
         this.badbaques.children.iterate((badbaca) => {
-            badbaca.setPosition(badbaca.x - this.move, badbaca.y);
+            badbaca.setPosition(badbaca.x - badbaca.move, badbaca.y);
             if (badbaca.x < -50) {
-                this.move = -1
-                badbaca.flipX = true;
+                badbaca.move *= -1
+                badbaca.scaleX = -0.2;
             }
             else if (badbaca.x > 1100) {
-                this.move = 1;
-                badbaca.flipX = false;
+                badbaca.move *= -1;
+                badbaca.scaleX = 0.2;
+            }
+        });
+
+        this.baques.children.iterate((baca) => {
+            baca.setPosition(baca.x - baca.move, baca.y);
+            if (baca.x < -50) {
+                baca.move *= -1
+                baca.scaleX = -0.2;
+            }
+            else if (baca.x > 1100) {
+                baca.move *= -1;
+                baca.scaleX = 0.2;
             }
         });
 
 
 
+    }
+
+    craerBadBaca() {
+        console.log("Rula");
+        let randomY = Phaser.Math.Between(500, 750);
+        console.log("Random Y: " + randomY)
+        this.badbaques.create(1100, randomY, 'badbaca').setScale(0.2);
+        let badbaca = this.badbaques.getLast(true);
+        badbaca.setInteractive();
+        badbaca.move = Phaser.Math.Between(1, 5);;
+        badbaca.on('pointerup', () => this.matarBaca('badbaca', badbaca))
+        if (randomY >= 500 && randomY <= 600) {
+            badbaca.setDepth(2);
+        }
+        else {
+            badbaca.setDepth(5);
+        }
+    }
+
+    crearBaca() {
+        let randomY = Phaser.Math.Between(500, 800);
+        this.baques.create(1100, randomY, 'baca').setScale(0.2);
+        let baca = this.baques.getLast(true);
+        baca.setInteractive();
+        baca.move = Phaser.Math.Between(1, 5);
+        baca.on('pointerup', () => this.matarBaca('baca', baca))
+        if (randomY >= 500 && randomY <= 600) {
+            baca.setDepth(2);
+        }
+        else {
+            console.log("Entro");
+            baca.setDepth(5);
+        }
     }
 
     shoot() {
@@ -113,6 +147,19 @@ class GameScene extends Phaser.Scene {
                 this.textPoints.setText("Punts: " + this.points);
             }
             obj.destroy();
+
+            let randomNumBaques = Phaser.Math.Between(3, 5);
+
+            for (var i = 0; i < randomNumBaques; i++) {
+                let prob = Phaser.Math.Between(1, 2);
+                if (prob == 1) {
+                    this.crearBaca();
+                }
+                else {
+                    this.craerBadBaca();
+                }
+            }
+
         }, 100);
     }
 }
